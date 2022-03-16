3d7621c4eb7853f8b745ccd276ab4f16237ec1de
Artemiza Rocha <miza-pma@hotmail.com>
2022-03-16 19:30:33 -0300

    Inclusão do projeto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Jogo_de_RPG.src.Entities;
+
+namespace Jogo_de_RPG
+{
+     class Program
+{
+      static void Main(string[] args)
+      {
+             Knight arus = new Knight("Arus", 23, "knight");
+             Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+             Ninja Ninja = new Ninja("Wedge", 29, "Ninja");
+             Topapa Topapa = new Topapa("Topapa", 29, "Black Wizard");
+            
+
+             
+               Console.WriteLine(arus.Attack());
+               Console.WriteLine(Ninja.Attack());
+               Console.WriteLine(Topapa.Black());
+              
+              
+               
+            }
+     }
+}
+
+
+
+
+
+
+
+
